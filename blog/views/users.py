90d0d8ee0fb2156e4bf5a9df0315bc26014c3581
Alina <alinayanzorkina@gmail.com>
2023-01-12 18:44:06 +0300

    3 commit

--- blog/views/users.py
+++ blog/views/users.py
@@ -1,23 +1,25 @@
 from flask import Blueprint, render_template
 from werkzeug.exceptions import NotFound
+from blog.models import User
 
 users_app = Blueprint("users_app", __name__)
-USERS = {
-    1: "Alina",
-    2: "Oleg",
-    3: "Jared",
-}
 
 
 @users_app.route("/", endpoint="list")
 def users_list():
-    return render_template("users/list.html", users=USERS)
+    users = User.query.all()
+    return render_template("users/list.html", users=users)
 
 
 @users_app.route("/<int:user_id>/", endpoint="details")
 def user_details(user_id: int):
-    try:
-        user_name = USERS[user_id]
-    except KeyError:
+    user = User.query.filter_by(id=user_id).one_or_none()
+    if user is None:
         raise NotFound(f"User #{user_id} doesn't exist!")
-    return render_template('users/details.html', user_id=user_id, user_name=user_name)
+
+    return render_template("users/details.html", user=user)
+
+
+
+
+
--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,39 @@
 from blog.app import app
+from blog.models.database import db
+from blog.models import User
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         debug=True,
     )
+
+
+@app.cli.command("init-db")
+def init_db():
+    """
+    Run in your terminal:
+    flask init-db
+    """
+    db.create_all()
+    print("done!")
+
+
+@app.cli.command("create-users")
+def create_users():
+    """
+    Run in your terminal:
+    flask create-users
+    > done! created users: <User #1 'admin'> <User #2 'james'>
+    """
+
+    admin = User(username="admin", is_staff=True)
+    james = User(username="james")
+    db.session.add(admin)
+    db.session.add(james)
+    db.session.commit()
+    print("done! created users:", admin, james)
+
+
+
+
