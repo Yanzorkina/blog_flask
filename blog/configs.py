2c35b7e215b834ea40a90f73f1a1f92fcf60e3ec
Alina <alinayanzorkina@gmail.com>
2023-02-03 18:18:28 +0300

    9 commit

--- blog/configs.py
+++ blog/configs.py
@@ -6,7 +6,7 @@ FLASK_ADMIN_SWATCH = 'vapor'
 class BaseConfig(object):
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///C:\\Users\\olegy\\Desktop\\Blog\\blog.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "abcdefg123456"
     WTF_CSRF_ENABLED = True
@@ -14,8 +14,13 @@ class BaseConfig(object):
 
 class DevConfig(BaseConfig):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
+
+
+OPENAPI_URL_PREFIX = '/api/swagger'
+OPENAPI_SWAGGER_UI_PATH = '/'
+OPENAPI_SWAGGER_UI_VERSION = '3.22.0'
